8634a4ef283faecbe363becbbae3a36363e67481
Christos Bramis <bramis04chris@gmail.com>
2026-06-13 22:34:40 +0300

    New example pipelines

---
+++ pipelines/example_pipeline_with_compiler.py
@@ -0,0 +1,24 @@
+import os
+from kfp import dsl
+
+# Example pipeline code that has COMPILER and wants to use Humaine Image (humaineImage=true by default)
+
+@dsl.component(base_image="python:3.13-slim")
+def train_model_op():
+    import pandas as pd
+    from sklearn.datasets import make_classification
+    # ... code ...
+    print("Training component running....")
+
+@dsl.pipeline(name='pipeline-with-compiler-and-humaine-image')
+def my_pipeline(): 
+    train_task = train_model_op()
+
+if __name__ == "__main__":
+    from kfp import compiler
+    import sys
+     
+    compiler.Compiler().compile(
+        pipeline_func=my_pipeline,
+        package_path='pipeline_temp.yaml'
+    )
---
+++ pipelines/example_pipeline_without_compiler.py
@@ -0,0 +1,15 @@
+import os
+from kfp import dsl
+
+# Example pipeline code that does NOT have COMPILER and does NOT want to use Humaine Image (humaineImage=false)
+
+@dsl.component(base_image="python:3.13-slim", humaineImage=False)
+def train_model_op():
+    import pandas as pd
+    from sklearn.datasets import make_classification
+    # ... code ...
+    print("Training component running....")
+
+@dsl.pipeline(name='pipeline-without-compiler-and-humaine-image')
+def my_pipeline(): 
+    train_task = train_model_op()
